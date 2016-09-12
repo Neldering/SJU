22e737759e4759e71daa1cb7f472ce7d0e5121b2
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-12 13:11:58 -0400

    Create PrintlnPrint.java

---
+++ PrintlnPrint.java
@@ -0,0 +1,25 @@
+/*
+   Println Print
+   Nicolas Eldering
+   9/9/2016
+   Part 1 Homework assignment 1
+   This program prints "This is a test of the emergency broadcast system" using different println's and print statements
+   The main method prints the statements 
+*/
+
+public class PrintlnPrint 
+{
+   public static void main( String[] args ) 
+   {
+  
+   System.out.print("This is a test");
+   System.out.println();
+   System.out.print("of the emergency");
+   System.out.println();
+   System.out.print("broadcast ");
+   System.out.print("system");
+   
+   System.out.println("This is a test\n of the emergency");
+   System.out.print("broadcast system");
+   }
+}
