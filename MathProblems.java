67fd9f545c4e33c0ebdcb0ccd9258fda65040ba6
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-12 14:12:39 -0400

    Create MathProblems.java

---
+++ MathProblems.java
@@ -0,0 +1,19 @@
+public class MathProblems 
+{
+   public static void main( String[] args ) 
+   {
+   
+   float A = 2;
+   float B = 2;
+   float C = 2; 
+   
+   float Problem1 = 2(A*A)+4*A+29;
+   float Problem2 = (4C+A*C)/(3*B);
+   
+   System.out.println("The anwser to 2(A*A)+4*A+29 when A is equal to: " + A + " is: " + Problem1);
+   System.out.println("The anwser to (4C+A*C)/(3*B) when A is equal to: " + A + ", B is equal to: " + B + " and C is equal to: " + C);
+   System.out.println("is: " + Problem2);
+   
+   
+   }
+}
