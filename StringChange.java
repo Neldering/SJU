c0fe7ac4415994da526b34f26e50aa9d63fa5b2d
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-20 15:01:19 -0400

    Update StringChange.java

--- StringChange.java
+++ StringChange.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class StringChange {
+public class StringChange 
+{
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
         
-      Scanner kb = new Scanner(System.in);
-      String originalString, newString;
+    Scanner kb = new Scanner(System.in);
+    String originalString, newString;
       
         
-      System.out.println("Please enter the string to be changed (It must contain the word hate):");
-      originalString = kb.nextLine();
-      newString = originalString.replaceFirst("hate", "love");
-      System.out.println("I have edited that line to read:");
-      System.out.println(newString);
-    }
+    System.out.println("Please enter the string to be changed (It must contain the word hate):");
+    originalString = kb.nextLine();
+    newString = originalString.replaceFirst("hate", "love");
+    System.out.println("I have edited that line to read:");
+    System.out.println(newString);
+  }
 
 }
