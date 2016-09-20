fc6465c758b3839e2f73e048f7c5ff8c1468940b
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-20 14:59:28 -0400

    Update StringChange.java

--- StringChange.java
+++ StringChange.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
   public static void main(String[] args) {
         
       Scanner kb = new Scanner(System.in);
+      String originalString, newString;
+      
         
       System.out.println("Please enter the string to be changed (It must contain the word hate):");
-      String originalString = kb.nextLine();
+      originalString = kb.nextLine();
       newString = originalString.replaceFirst("hate", "love");
       System.out.println("I have edited that line to read:");
       System.out.println(newString);
