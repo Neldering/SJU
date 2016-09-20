e7da846d6a0b96ad02a216c5c002d9b3d935acdd
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-20 15:00:32 -0400

    Update CommuterCostCalculator.java

--- CommuterCostCalculator.java
+++ CommuterCostCalculator.java
@@ -1,7 +1,9 @@
 import java.Util.Scanner;
 
-public class CommuterCostCalculator { 
-  public static void main(String[] args) { 
+public class CommuterCostCalculator 
+{ 
+  public static void main(String[] args) 
+  { 
     
     
     System.out.println("This program will calculate how much your commute cost."); 
