347957d735fd7dc354e4f8847232abb6e28361dc
Nicolas Eldering <Neldering@users.noreply.github.com>
2016-09-20 14:58:25 -0400

    Update StringChange.java

--- StringChange.java
+++ StringChange.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
       System.out.println("Please enter the string to be changed (It must contain the word hate):");
       String originalString = kb.nextLine();
       newString = originalString.replaceFirst("hate", "love");
-      System.out.println("I have rephrased that line to read:");
+      System.out.println("I have edited that line to read:");
       System.out.println(newString);
     }
 
